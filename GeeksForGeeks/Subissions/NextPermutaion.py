69cf2081a2bbdf25615d83e1143c817af98fa53f
YenIndra <100924093+YenIndra@users.noreply.github.com>
2022-03-17 14:25:25 +0530

    Create NextPermutaion.py
    given a array you have to find the next permutaion

---
+++ GeeksForGeeks/Subissions/NextPermutaion.py
@@ -0,0 +1,23 @@
+ def nextPermutation(self, N, arr):
+        # code here
+        k = -1
+        l = 0
+        i = N -1
+        while(i>0):
+            if arr[i-1] < arr[i]:
+                k = i-1
+                break
+            i-=1
+        if k == -1:
+            return arr[::-1]
+        i = N-1
+        while(i>k):
+            if arr[i] > arr[k]:
+                l = i
+                break
+            i-=1
+        arr[l],arr[k] = arr[k],arr[l]
+        
+        newarr = arr[0:k+1] + arr[k+1:][::-1]
+        #print(newarr)
+        return newarr
