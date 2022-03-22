14c85b2b854f72ef5048277194d986501b6a8d83
YenIndra <100924093+YenIndra@users.noreply.github.com>
2022-03-22 19:58:31 +0530

    Create countInversion.py

---
+++ Arrays/countInversion.py
@@ -0,0 +1,44 @@
+def merge(arr, temp_arr, left, mid, right):
+    i = left     # Starting index of left subarray
+    j = mid + 1  # Starting index of right subarray
+    k = left     # Starting index of to be sorted subarray
+    inv_count = 0
+
+    # Conditions are checked to make sure that
+    # i and j don't exceed their
+    # subarray limits.
+
+    while i <= mid and j <= right:
+
+        # There will be no inversion if arr[i] <= arr[j]
+
+        if arr[i] <= arr[j]:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            # Inversion will occur.
+            temp_arr[k] = arr[j]
+            inv_count += (mid-i + 1)
+            k += 1
+            j += 1
+
+    # Copy the remaining elements of left
+    # subarray into temporary array
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        k += 1
+        i += 1
+
+    # Copy the remaining elements of right
+    # subarray into temporary array
+    while j <= right:
+        temp_arr[k] = arr[j]
+        k += 1
+        j += 1
+
+    # Copy the sorted subarray into Original array
+    for loop_var in range(left, right + 1):
+        arr[loop_var] = temp_arr[loop_var]
+
+    return inv_count
